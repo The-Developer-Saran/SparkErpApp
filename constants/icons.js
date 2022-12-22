ad02c1f588629e89c59358916566427295dbb28b
The-Developer-Saran <111742963+The-Developer-Saran@users.noreply.github.com>
2022-12-22 10:35:34 +0530

    Add files via upload

---
+++ constants/icons.js
@@ -0,0 +1,17 @@
+import dashboard from './images/DashBoard.png'
+import holidays from './images/Holidays.png'
+import notifications from './images/Notifications.png'
+import attendencereports from './images/AttendenceReports.png'
+import LeaveRequest from './images/LeaveRequest.png'
+import lessthan from './images/LessThan.png'
+import location from './images/Location.png'
+
+export default{
+    dashboard,
+    holidays,
+    notifications,
+    attendencereports,
+    LeaveRequest,
+    lessthan,
+    location,
+};
